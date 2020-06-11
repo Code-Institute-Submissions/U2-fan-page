dcfdd0c8a18ae1452b3390fa2a40688577488c8a
David  Beamish <davidrbeamish@gmail.com>
2020-06-11 13:45:50 +0000

    tried to add a loop to display thr array info for usStadiums but hasnt worked yet

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -205,3 +205,86 @@ document.getElementById('RW-drop').onclick = function(){
 }
 
 
+
+
+var usStadiums = [
+    {
+      name: "Madison Square Gardens", city: "New York", date: '1st of June 2021', lat: 40.7505, lng: -73.9934
+    },
+    {
+      name: "Scotiabank Arena", city: "Toronto", date: '8th of June 2021', lat: 43.6435, lng: -79.3791
+    },
+    {
+      name: "Boston Garden", city: "Boston", date: '12th of June 2021', lat: 42.3662, lng: -71.0621
+    },
+    {
+      name: "Wells Fargo Arena", city: "Philadelphia", date: '28nd of June 2021', lat: 39.9012, lng: -75.1720
+    },
+    {
+      name: "Bridgestone Arena", city: "Nashville", date: '22nd of June 2021', lat: 36.1592, lng: -86.7785
+    },
+    {
+      name: "United Center", city: "Chicago", date: '28th of June 2021', lat: 41.8807, lng: -87.6742
+    },
+    {
+      name: "Key Arena", city: "Seattle", date: '30th of June 2021', lat: 47.6221, lng: -122.3540
+    },
+    {
+      name: "Pepsi Center", city: "Denver", date: '3rd of July 2021', lat: 39.7487, lng: -105.0077
+    },
+    {
+      name: "SAP center", city: "San Jose", date: '10th of July 2021', lat: 37.3328, lng: -121.9012
+    },
+    {
+      name: "the Collusem", city: "Los Angeles", date: '30th of July 2021', lat: 34.0141, lng: -118.2879
+    }
+  ];
+var euStadiums = [
+    {
+      name: "Croke Park", city: "Dublin", date: '21st of August 2021', lat: 53.3607, lng: -6.2512
+    },
+     {
+      name: "Thomond Park", city: "Dublin", date: '22nd of August 2021', lat: 52.6742, lng: -8.6425
+    },
+    {
+      name: "Twickenham Stadium", city: "London", date: '25th of August 2021', lat: 51.4546, lng: -0.3410
+    },
+    {
+      name: "City of Manchester Stadium", city: "Manchester", date: '28th of August 2021', lat: 53.4831, lng: -2.2004
+    },
+    {
+      name: "Millennium stadium", city: "Cardiff", date: '30th of August 2021', lat: 51.4782, lng: -3.1826
+    },
+    {
+      name: "Olympiastadion Berlin", city: "Berlin", date: '3rd of September 2021', lat: 52.5147, lng: 13.2395
+    },
+    {
+      name: "Stade De France", city: "Paris", date: '10th of September 2021', lat: 48.9245, lng:  2.3602
+    },
+    {
+      name: "Johan Cruijff ArenA", city: "Amsterdam", date: '14th of September 2021', lat: 52.3144, lng:  4.9420
+    },
+    {
+      name: "Santiago Bernabéu Stadium", city: "Madrid", date: '19th of September 2021', lat: 40.4531, lng:  -3.6883
+    },
+    {
+      name: "Stadio Olimpico", city: "Rome", date: '30th of September 2021', lat: 41.9341, lng: 12.4547
+    }
+  ];
+
+
+    document.getElementById('US-drop').onclick = function() {
+        let stadiumName;
+        for ( i=0; i<usStadiums.length; i++) {
+            stadiumName = "location " + usStadiums[i].name;
+        }
+        document.getElementById("lists").write = stadiumName;
+    }
+
+
+
+
+
+
+//   document.getElementsByClassName('US-list').onclick = function(){
+//     return stadiumName;
